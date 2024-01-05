4b60c2042c6e74f738d59809a0042dda2bb8b1bd
Sarika Nautiyal <nsarika2002@gmail.com>
2024-01-05 22:34:26 +0530

    create patch file

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
-
-/**
+/*
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
-
 /**
  * The parent element of the react app.
  * It renders title, button and Graph react element.
@@ -17,38 +16,45 @@ interface IState {
 class App extends Component<{}, IState> {
   constructor(props: {}) {
     super(props);
-
     this.state = {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
-
   /**
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data} />)
+    }
   }
-
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
-
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
+    }
   /**
    * Render the App react component
    */
   render() {
-    return (
-      <div className="App">
+      return(
+      <div className = "App" >
         <header className="App-header">
           Bank & Merge Co Task 2
         </header>
@@ -66,9 +72,8 @@ class App extends Component<{}, IState> {
             {this.renderGraph()}
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
-
 export default App;
